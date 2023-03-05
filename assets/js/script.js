4c946d7cfbeec159ac969af65bef9fcd128ee599
Gerry Paul <gpaul12@nc.rr.com>
2023-03-05 15:27:40 -0500

    js final touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,18 +59,20 @@ searchButton.click(function () {
             url: urlFiveDay,
             method: "GET"
         }).then(function (response) {
+            // Array for 5-days 
             var day = [0, 8, 16, 24, 32];
             var fiveDayCard = $(".fiveDayCard").addClass("card-body");
             var fiveDayDiv = $(".fiveDayOne").addClass("card-text");
             fiveDayDiv.empty();
-            
+            // For each for 5 days
             day.forEach(function (i) {
-                var fiveDayTimeUTC1 = new Date(response.list[i].dt * 1000);
-                fiveDayTimeUTC1 = fiveDayTimeUTC1.toLocaleDateString("en-US");
+                var FiveDayTimeUTC1 = new Date(response.list[i].dt * 1000);
+                FiveDayTimeUTC1 = FiveDayTimeUTC1.toLocaleDateString("en-US");
 
-                fiveDayDiv.append("<div class=fiveDayColor>" + "<p>" + fiveDayTimeUTC1 + "</p>" + '<img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png">' + "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" + "</div>");
+                fiveDayDiv.append("<div class=fiveDayColor>" + "<p>" + FiveDayTimeUTC1 + "</p>" + `<img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png">` + "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" + "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" + "</div>");
 
-            });
+
+            })
         });
     };
 });
