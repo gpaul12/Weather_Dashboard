b626ecea634913215c803bcb0a1bb0d7f62f77be
Gerry Paul <gpaul12@nc.rr.com>
2023-03-05 14:34:58 -0500

    js var city, city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,7 @@ var searchButton = $(".searchButton");
 var apiKey = "a645e448d6e973e7ad29672398979fba";
 
 for (var i = 0; i < localStorage.length; i++) {
-    
+    var city = localStorage.getItem(i)
+   var cityName = $(".list-group").addClass("list-group-item");
+   cityName.append("<li>" + city + "</li>");
 }
