21a48fa1548a0076fd6e6768f94b1a84068b0793
Gerry Paul <gpaul12@nc.rr.com>
2023-03-05 14:56:07 -0500

    add temp

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,9 @@ searchButton.click(function () {
             var timeUTC = new Date(response.dt * 1000);
             currentName.append(response.name + " " + timeUTC.toLocaleDateString("en-US"));
             currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+
+            var currentTemp = currentName.append("<p>");
+
             
         })
     }
