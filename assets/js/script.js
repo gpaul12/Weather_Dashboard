11444258b55262f4fa68825eb16b76ecf323b2ce
Gerry Paul <gpaul12@nc.rr.com>
2023-03-05 15:03:28 -0500

    uv index url

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,8 @@ searchButton.click(function () {
 
             currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
 
-            
+            var urlUV = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
+
 
         })
     }
