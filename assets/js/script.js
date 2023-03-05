f96cd6aa1ba28b531b2d1e5513561ef8811d27fe
Gerry Paul <gpaul12@nc.rr.com>
2023-03-05 14:47:10 -0500

    list-group append an li

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,5 +14,15 @@ searchButton.click(function () {
     var searchInput = $(".searchInput").val();
     var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
     var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
-    
+
+    if (searchInput == "") {
+    } else {
+        $.ajax({
+            url: urlCurrent,
+            method: "GET"
+        }).then(function (response) {
+            var cityName = $(".list-group").addClass("list-group-item");
+            cityName.append("<li>" + response.name + "</li>");
+        })
+    }
 })
